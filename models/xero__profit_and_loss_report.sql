492e2ad6c502f47539a348fe1938e91abf86e39c
fivetran-reneeli <renee.li@fivetran.com>
2021-10-28 16:34:25 -0500

    update models with source relation

--- models/xero__profit_and_loss_report.sql
+++ models/xero__profit_and_loss_report.sql
@@ -18,12 +18,13 @@ with calendar as (
         ledger.account_code,
         ledger.account_type, 
         ledger.account_class, 
+        ledger.source_relation, 
         coalesce(sum(ledger.net_amount * -1),0) as net_amount
     from calendar
     left join ledger
         on calendar.date_month = cast({{ dbt_utils.date_trunc('month', 'ledger.journal_date') }} as date)
     where ledger.account_class in ('REVENUE','EXPENSE')
-    {{ dbt_utils.group_by(7) }}
+    {{ dbt_utils.group_by(8) }}
 
 )
 

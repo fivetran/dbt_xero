85abd542dc2e6dba8ddbe06fb68ab782d1359a7b
Avinash Kunnath <avinash.kunnath@fivetran.com>
2025-03-28 10:28:52 -0700

    Revert scripts/docs/workflows to main

--- .buildkite/scripts/run_models.sh
+++ .buildkite/scripts/run_models.sh
@@ -21,6 +21,6 @@ dbt run --target "$db" --full-refresh
 dbt test --target "$db"
 dbt run --vars '{xero__using_credit_note: false, xero__using_bank_transaction: false}' --target "$db" --full-refresh
 dbt test --target "$db"
-dbt run --target "$db" --full-refresh 
+dbt run --target "$db" --full-refresh
 dbt test --target "$db"
 dbt run-operation fivetran_utils.drop_schemas_automation --target "$db"
